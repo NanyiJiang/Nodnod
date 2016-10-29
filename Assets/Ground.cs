631b19935586010b651924987d98ee487f6f7fdd
EthanLi-LAPTOPW\Ethan Li <yf8li@uwaterloo.ca>
2016-10-29 17:42:25 -0400

    Added Light Grid

---
+++ Assets/Ground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour
+{
+    public float TextureScale = 20.0f;
+	// Use this for initialization
+	void Start ()
+	{
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.mainTextureScale = new Vector2(TextureScale, TextureScale);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
